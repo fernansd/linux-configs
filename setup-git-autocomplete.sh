47d1667f4761912a8e926073094245aff37bb764
Fernando S <code@fernansd.dev>
2023-08-01 12:51:55 +0200

    Added script to install Git autocomplete in Ubuntu

---
+++ setup-git-autocomplete.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+function check_error() {
+	if [[ $? -ne 0 ]]; then
+		echo "$1"
+		exit 1
+	fi
+}
+
+grep -E "^ID=ubuntu$" /etc/os-release
+check_error "Error. Solc funciona para Ubuntu"
+
+sudo apt install -y bash-completion
+check_error "Error. No se ha podido instalar el paquete 'bash-completion'"
+
+mkdir -p ~/.local/share
+check_error "Error. No se ha podido crear la carpeta correspondiente"
+
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -P ~/.local/share
+check_error "Error. No se pudo descargar el script de autocompletado"
+
+cat >> ~/.bashrc <<'EOF'
+if [ -f ~/.local/share/git-completion.bash ]; then
+	source ~/.local/share/git-completion.bash
+fi
+EOF
